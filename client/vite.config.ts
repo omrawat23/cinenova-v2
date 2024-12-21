e557391044a07c5a75d5ef8d21b6bf4461d2919d
omrawat23 <omrawat23@gmail.com>
2024-12-21 22:13:22 +0530

    .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
@@ -9,13 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    target: "esnext",
-    sourcemap: false,
-  },
-  esbuild: {
-    logOverride: {
-      'ts-resolver-error': 'silent'
-    }
-  }
-});
+})
