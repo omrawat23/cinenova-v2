bc9e7f5328263e4268ef0e3c9225bc9cbad3c280
omrawat23 <omrawat23@gmail.com>
2025-01-08 13:36:42 +0530

    vercel speed insights added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from './App.tsx';
 import './index.css';
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const queryClient = new QueryClient();
 
