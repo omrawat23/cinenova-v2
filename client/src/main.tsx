da807e1644d706b8c466d932e8835e2ffe5a08dc
omrawat23 <omrawat23@gmail.com>
2025-01-08 13:32:48 +0530

    speed insights added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from './App.tsx';
 import './index.css';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <QueryClientProvider client={queryClient}>
      <Analytics />
+     <SpeedInsights />
        <App />
      </QueryClientProvider>
   </StrictMode>
