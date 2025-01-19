66abb75fe9b97d3d45e7bca788a005badc6e349b
omrawat23 <omrawat23@gmail.com>
2025-01-19 16:51:54 +0530

    .

--- client/src/components/PlusWatchlistButton.tsx
+++ client/src/components/PlusWatchlistButton.tsx
@@ -64,6 +64,7 @@ export default function PlusWatchlistButton({
         }
       }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [user, currentMovie, currentTv, setWatchlist]
   );
 
